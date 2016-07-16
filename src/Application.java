822fcdeec5f5e0c99c407dc495b89c36449651bd
partypiet <pschuerk@web.de>
2016-07-16 12:28:17 +0200

    testCommit

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,10 @@
 
 public class Application {
     public static void main(String[] args) {
-        if (args.length > 0)
+        if (args.length > 0) {
             System.out.print(args[0] + " + 1 = ");
             System.out.println(computeInputPlusOne(Integer.parseInt(args[0])));
+        }
     }
 
     public static int computeInputPlusOne(int number) {
