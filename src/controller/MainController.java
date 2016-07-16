9ea61daa1dfde25a75e6d76cbfa01a0c3e8ab239
Lukas <lukaspulle@gmail.com>
2016-07-16 22:19:27 +0200

    inplemented inputcontroller to maincontroller

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -4,11 +4,13 @@ public class MainController {
 	private ShipController shipController;
 	private ProjectileController projectileController;
 	private CollisionController collisionController;
+	private InputController inputController;
 	
 	public MainController() {
 		shipController = new ShipController(this);
 		projectileController = new ProjectileController(this);
 		collisionController = new CollisionController(this);
+		inputController = new InputController(this);
 	}
 	
 	public ShipController getShipController() {
@@ -21,5 +23,9 @@ public class MainController {
 
 	public CollisionController getCollisionController() {
 		return collisionController;
+	}
+	
+	public InputController getInputController(){
+		return inputController;
 	}
 }
