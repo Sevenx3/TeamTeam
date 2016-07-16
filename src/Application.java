4b5ef565e75c046758c655c575d4b5497ca72cb4
partypiet <pschuerk@web.de>
2016-07-16 12:22:16 +0200

    testCommit

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("#DoHack!2016");
+        if (args.length > 0)
+            System.out.println(computeInputPlusOne(Integer.parseInt(args[0])));
+    }
+
+    public static int computeInputPlusOne(int number) {
+        return number + 1;
     }
 }
