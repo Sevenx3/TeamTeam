991d4a7d159ba059e04fc62d9433b0bebe93ba70
Lukas <lukaspulle@gmail.com>
2016-07-16 22:12:56 +0200

    init Inputcontroller

---
+++ src/controller/InputController.java
@@ -0,0 +1,27 @@
+package controller;
+
+public class InputController {
+	
+	private MainController mainController;
+	
+	public InputController(MainController mainController){
+		this.mainController = mainController;
+	}
+
+	public void playerShipMoveLeft(){
+		
+	}
+	
+	public void playerShipMoveRight(){
+			
+	}
+	
+	public void playerShipMoveUp(){
+		
+	}
+	
+	public void playerShipMoveDown(){
+		
+	}
+	
+}
