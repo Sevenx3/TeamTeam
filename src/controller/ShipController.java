f947057380fbdb6f5a1bef31e2cbdf6c2ac3e958
partypiet <pschuerk@web.de>
2016-07-16 21:30:02 +0200

    MainController Controller Controller

--- src/controller/ShipController.java
+++ src/controller/ShipController.java
@@ -7,7 +7,11 @@ import model.ShipEnum;
 
 public class ShipController {
 	ArrayList<Ship> ships = new ArrayList<Ship>();
+	private MainController mainController;
 	
+	public ShipController(MainController maincontroller) {
+		this.mainController = mainController;
+	}
 	public void createNewShip(int x, int y, ShipEnum type) {
 		Ship newShip = new Ship(x, y, type);
 		ships.add(newShip);
