7032f4f519d73073e1e69c8b0e77d2456823afa2
Sevenx3 <marchoefken@gmx.de>
2016-07-16 12:25:08 +0200

    test

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,14 @@
 
 public class Application {
 	
+	private static final int ARGUMENTS = 2;
+	
     public static void main(String[] args) {
         if (args.length > 0)
             System.out.println(computeInputPlusOne(Integer.parseInt(args[0])));
+        if (args.length > ARGUMENTS){
+        	
+        }
     }
 
     public static int computeInputPlusOne(int number) {
